86f5644e905403f767db44732a25eec708b48b3a
MateusHBR <mateusmorais.r@gmail.com>
2023-05-14 22:34:07 -0300

    add async database connection with sqlx with database migrations

--- README.md
+++ README.md
@@ -12,3 +12,19 @@ cargo watch -x run
 ```
 cargo build --release
 ```
+
+Get database ipAddress:
+
+```
+docker ps
+```
+
+Get the ps responsible for the service that i want to get the ipAddress with:
+
+```
+docker inspect <ps-id-here> | grep IPAddress
+```
+
+SQLX-CLI
+This cli is necessary for managing databases, migrations, and more...
+https://github.com/launchbadge/sqlx/tree/main/sqlx-cli
--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,28 @@ mod models;
 
 use cors::*;
 use handlers::*;
+use sqlx::postgres::PgPoolOptions;
+use std::env;
 
 #[launch]
 async fn rocket() -> _ {
+    pretty_env_logger::init();
+    dotenvy::dotenv().ok();
+
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&env::var("DATABASE_URL").expect("DATABASE_URL must be set."))
+        .await
+        .unwrap();
+
+    let recs = sqlx::query!("SELECT * FROM questions")
+        .fetch_all(&pool)
+        .await
+        .unwrap();
+
+    info!("********* Question Records *********");
+    info!("{:?}", recs);
+
     rocket::build()
         .mount(
             "/",
